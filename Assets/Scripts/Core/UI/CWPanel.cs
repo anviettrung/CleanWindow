0bb8665cf66fa9b4a5b947c4dfcbaf1832fa4802
anviettrung <an.viettrung.m@gmail.com>
2020-02-21 15:10:25 +0700

    Feature: UI Panel layout

---
+++ Assets/Scripts/Core/UI/CWPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CWPanelEvent : UnityEvent<CWPanel> { }
+
+public class CWPanel : MonoBehaviour
+{
+	public CWPanelEvent onEnablePanel = new CWPanelEvent();
+	public CWPanelEvent onDisablePanel = new CWPanelEvent();
+
+	public void OnEnable()
+	{
+		onEnablePanel.Invoke(this);
+	}
+
+	public void OnDisable()
+	{
+		onDisablePanel.Invoke(this);
+	}
+}
---
+++ Assets/Scripts/Core/UI/CWPanelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CWPanelManager : Singleton<CWPanelManager>
+{
+	public List<CWPanel> cwPanels;
+	public GameObject blackCover;
+
+	private void Awake()
+	{
+		foreach (CWPanel pan in cwPanels) {
+			pan.onEnablePanel.AddListener(OnOpenPanel);
+			pan.onDisablePanel.AddListener(OnClosePanel);
+		}
+	}
+
+	public void OnOpenPanel(CWPanel panel)
+	{
+		HideAllPanelsExcept(panel);
+		blackCover.SetActive(true);
+	}
+
+	public void OnClosePanel(CWPanel panel)
+	{
+		blackCover.SetActive(false);
+	}
+
+	protected void HideAllPanelsExcept(CWPanel panel)
+	{
+		foreach (CWPanel pan in cwPanels) {
+			if (panel != pan)
+				pan.gameObject.SetActive(false);
+		}
+	}
+}
