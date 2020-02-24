09ccc9826ce1bdf73c60f49de1dd3aac3c5c805a
anviettrung <an.viettrung.m@gmail.com>
2020-02-24 15:04:43 +0700

    ui tab highlight

---
+++ Assets/Scripts/Core/UI/UITabHighlight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITabHighlight : MonoBehaviour
+{
+	[Header("References")]
+	public Image image;
+	public Sprite spriteHighlightOn;
+	public Sprite spriteHighlightOff;
+
+	[SerializeField] protected bool isOn;
+
+	public void SwitchStatus()
+	{
+		isOn = !isOn;
+	}
+
+	public void SetStatus(bool s)
+	{
+		isOn = s;
+	}
+
+	public void Update()
+	{
+		if (isOn)
+			image.sprite = spriteHighlightOn;
+		else
+			image.sprite = spriteHighlightOff;
+	}
+}
