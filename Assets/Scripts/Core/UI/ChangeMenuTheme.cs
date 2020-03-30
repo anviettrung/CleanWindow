640f0d35c6ccc75c61bee0b50de9ce731098b577
Hung Dang <hungk28tlu@gmail.com>
2020-03-30 18:45:45 +0700

    change theme main menu

---
+++ Assets/Scripts/Core/UI/ChangeMenuTheme.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMenuTheme : MonoBehaviour
+{
+    [Header("Theme Data")]
+    public ListThemeData themeDatas;
+
+    [Header("Home Background")]
+    public SpriteRenderer walll;
+    public SpriteRenderer furniture;
+
+    public void OnClickChangeTheme(int themeID)
+    {
+        this.walll.sprite = this.themeDatas.Theme[themeID - 1].WallSprite;
+        this.furniture.sprite = this.themeDatas.Theme[themeID - 1].FurnitureSprite;
+    }
+}
