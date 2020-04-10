754167bd9a056d15f72d6f4f1fd9d5d10fb9d32c
Hung Dang <hungk28tlu@gmail.com>
2020-04-10 22:29:08 +0700

    script UI tool

---
+++ Assets/Scripts/Core/UI/UIBackgroundShop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class UIBackgroundShop : MonoBehaviour
+{
+    public Image background;
+
+    private void Start()
+    {
+        var rect = background.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+}
---
+++ Assets/Scripts/Core/UI/UIHighLightText.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIHighLightText : MonoBehaviour//, ISelectHandler, IDeselectHandler
+{
+    public Color selectColor;
+    public Color deselectColor;
+    public Text mainText;
+    public GameObject deselectButton;
+    private UITabHighlight tab;
+
+    private void Start()
+    {
+        tab = GetComponent<UITabHighlight>();
+        if (tab.isOn)
+        {
+            mainText.color = selectColor;
+            deselectButton.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (tab.isOn)
+        {
+            mainText.color = selectColor;
+            deselectButton.SetActive(false);
+        }
+        else
+        {
+            mainText.color = deselectColor;
+            deselectButton.SetActive(true);
+        }
+    }
+
+    //public void OnSelect(BaseEventData eventData)
+    //{
+    //    mainText.color = selectColor;
+    //    deselectButton.SetActive(false);
+    //}
+
+    //public void OnDeselect(BaseEventData eventData)
+    //{
+    //    mainText.color = deselectColor;
+    //    deselectButton.SetActive(true);
+    //}
+}
