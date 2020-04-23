e30f0a84378f714d1cd38ff4903355da9f5e2268
Hung Dang <hungk28tlu@gmail.com>
2020-04-23 18:20:34 +0700

    Remove unused resouces
    Add new art

--- Assets/Scripts/Core/Manager/ConfigManager.cs
+++ Assets/Scripts/Core/Manager/ConfigManager.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class ConfigManager : Singleton<ConfigManager>
 {
     public SettingConfig settingConfig;
+    public GameNumberConfig gameNumberConfig;
 
     private void Awake()
     {
         this.LoadGameConfig();
     }
 
+    /// <summary>
+    /// Load all configs
+    /// </summary>
     private void LoadGameConfig()
+    {
+        this.LoadSettingConfig();
+        this.LoadGameNumberConfig();
+    }
+
+    /// <summary>
+    /// Setting config
+    /// </summary>
+    private void LoadSettingConfig()
     {
         var jsonContent = Resources.Load<TextAsset>("Configs/SettingConfig");
         this.settingConfig = JsonUtility.FromJson<SettingConfig>(jsonContent.text);
+    }
+
+    /// <summary>
+    /// Game number config
+    /// </summary>
+    private void LoadGameNumberConfig()
+    {
+        var jsonContent = Resources.Load<TextAsset>("Configs/GameNumberConfig");
+        this.gameNumberConfig = JsonUtility.FromJson<GameNumberConfig>(jsonContent.text);
     }
 }
 
@@ -23,4 +45,9 @@ public class SettingConfig
     public float Haptic_ContinuousIntensity;
     public float Haptic_ContinuousSharpness;
     public float Haptic_ContinuousDuration;
+}
+
+public class GameNumberConfig
+{
+    public float MaxGameToGetGiftBox;
 }
--- Assets/Scripts/Core/UI/UIProgress.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UIProgress : MonoBehaviour
-{
-    public Window.State state;
-    public GameObject highlight;
-}
