c80d4477c028885be25a67f8f3be27d090494cc9
Hung Dang <hungk28tlu@gmail.com>
2020-05-14 16:10:28 +0700

    GameAnalytic Setting

---
+++ Assets/Scripts/Core/Manager/GameAnalyticManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameAnalyticsSDK;
+
+public class GameAnalyticManager : Singleton<GameAnalyticManager>
+{
+    private void Awake()
+    {
+        GameAnalytics.Initialize();
+    }
+
+    public string Level
+    {
+        get
+        {
+            return "Level " + LevelManager.Instance.currentLevel.ToString();
+        }
+    }
+
+    public void StartLevel()
+    {
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, this.Level);
+        //Debug.Log("<color=red>GameAnalytic: Start</color>");
+    }
+
+    public void EndLevel()
+    {
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, this.Level);
+        //Debug.Log("<color=cyan>GameAnalytic: Complete</color>");
+    }
+}
